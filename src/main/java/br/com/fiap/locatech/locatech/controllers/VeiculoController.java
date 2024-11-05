3349b5a84a8bd7cf53b1b60c3eb1f9161a93cf1c
Diego Cruz <diegoaugustodacruz@gmail.com>
2024-11-05 09:56:19 -0300

    terminando controller, configurando banco h2

--- src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
@@ -3,13 +3,11 @@ package br.com.fiap.locatech.locatech.controllers;
 import br.com.fiap.locatech.locatech.entities.Veiculo;
 import br.com.fiap.locatech.locatech.services.VeiculoService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/veiculos")
@@ -23,11 +21,8 @@ public class VeiculoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Veiculo>> findAllVeiculos(
-            @RequestParam("page") int page,
-            @RequestParam("size") int size
-
-    ){
+    public ResponseEntity<List<Veiculo>> findAllVeiculos(@RequestParam("page") int page,
+                                                         @RequestParam("size") int size){
         log.info("Retornando veiculos");
         List<Veiculo> allVeiculo = veiculoService.findAllVeiculo(page, size);
 
@@ -35,15 +30,37 @@ public class VeiculoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Veiculo>> findVeiculo(
-            @PathVariable("id") Long id
-
-    ){
+    public ResponseEntity<Optional<Veiculo>> findVeiculo(@PathVariable("id") Long id){
         log.info("Retornando veiculo");
         Optional<Veiculo> veiculo = veiculoService.findVeiculoById(id);
 
         return ResponseEntity.ok(veiculo);
     }
 
+    @PostMapping
+    public ResponseEntity<Void> saveVeiculo(@RequestBody  Veiculo veiculo){
+        log.info("salvando veiculo");
+        veiculoService.saveVeiculo(veiculo);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateVeiculo(@PathVariable("id") Long id, @RequestBody Veiculo veiculo){
+        log.info("atualizando veiculo");
+        veiculoService.updateVeiculo(veiculo, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> updateVeiculo(@PathVariable("id") Long id){
+        log.info("deletando veiculo");
+        veiculoService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world";
+    }
 
 }
