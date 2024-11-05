e656608cb13ef2d50164c2307ded1f46d27713af
Diego Cruz <diegoaugustodacruz@gmail.com>
2024-11-05 07:36:22 -0300

    criando entidades

---
+++ src/main/java/br/com/fiap/locatech/locatech/entities/Aluguel.java
@@ -0,0 +1,26 @@
+package br.com.fiap.locatech.locatech.entities;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Aluguel {
+
+    private Long id;
+    private Long idPessoa;
+    private Long idVeiculo;
+    private String veiculoModelo;
+    private String pessoaCpf;
+    private String pessoaNome;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private BigDecimal valorTotal;
+
+}
