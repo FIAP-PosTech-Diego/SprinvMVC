03d7900094b87d88cfa6bae8d4414f9c8e69bd07
Diego Cruz <diegoaugustodacruz@gmail.com>
2024-11-06 07:11:14 -0300

    documentacao apis

--- src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
@@ -2,6 +2,9 @@ package br.com.fiap.locatech.locatech.controllers;
 
 import br.com.fiap.locatech.locatech.entities.Veiculo;
 import br.com.fiap.locatech.locatech.services.VeiculoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/veiculos")
 @Slf4j
+@Tag(name = "Veiculo", description = "Controller para veiculos")
 public class VeiculoController {
 
     private final VeiculoService veiculoService;
@@ -20,6 +24,13 @@ public class VeiculoController {
         this.veiculoService = veiculoService;
     }
 
+    @Operation(
+            description = "Busca todos veiculos paginados",
+            summary = "Busca de veiculos",
+            responses = {
+                    @ApiResponse(description = "ok", responseCode = "200")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<Veiculo>> findAllVeiculos(@RequestParam("page") int page,
                                                          @RequestParam("size") int size){
--- src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelDTO.java
+++ src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelDTO.java
@@ -1,10 +1,12 @@
 package br.com.fiap.locatech.locatech.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record AluguelDTO(
+        @Schema(description = "id da pessoa que esta alugando o veiculo")
         @NotNull(message = "Id da pessoa nao pode ser null")
         Long pessoaId,
         @NotNull(message = "Id do veiculo nao pode ser null")
